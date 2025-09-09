bcd21bb2dc7e798bce763d4506d4da27390b6921
sjh060623 <sjh060623@gmail.com>
2025-09-09 23:44:28 +0900

    first commit

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,13 @@
+import withPWA from "next-pwa";
+
+const isProd = process.env.NODE_ENV === "production";
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {};
 
-export default nextConfig;
+export default withPWA({
+  dest: "public",
+  disable: !isProd,
+  register: true,
+  skipWaiting: true,
+})(nextConfig);
